0e678cebe26a40b4cda5d730a7f31cf0648b64fc
ARIIK-ANTHONY <a.ariik@alustudent.com>
2025-05-26 02:17:09 +0200

    test_base_model.py

--- models/test_base_model.py
+++ models/test_base_model.py
@@ -1,6 +1,25 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
+def test_save():
+    try:
+        bm = BaseModel()
+        old_updated_at = bm.updated_at
+        bm.save()
+        new_updated_at = bm.updated_at
+        assert new_updated_at > old_updated_at
+        print("OK")
+    except Exception as e:
+        print("Failed:", e)
 
-my_model = BaseModel()
-print(my_model)
-print(my_model.to_dict())
+def test_to_dict():
+    try:
+        bm = BaseModel()
+        d = bm.to_dict()
+        assert type(d) is dict
+        assert "__class__" in d
+        assert type(d["created_at"]) is str
+        print("OK")
+    except Exception as e:
+        print("Failed:", e)
+
+# Call tests
+test_save()
+test_to_dict()
